4d2994f854c46ec06d2c1f3a9f6ec7a32edc1f5f
soham20008 <154255260+soham20008@users.noreply.github.com>
2024-01-10 22:38:58 +0530

    Upload from Projects

---
+++ perfect square.py
@@ -0,0 +1,8 @@
+import math as ma
+n = float(input("Type your integer greater than 1 "))
+b = (ma.sqrt(n))
+b = (b**2)
+if n == b:
+    print("It is a perfect square")
+else:
+    print("It is not a perfect square")
